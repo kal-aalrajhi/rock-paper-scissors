10982957a0c034ee3e50c940f1241027435abfc7
Kal <kal.aalrajhi@gmail.com>
2022-03-01 22:08:57 -0700

    Declare game options object

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
 // Global Variables
+var gameOptions = {
+    classicOptions: [
+        {
+            name: 'rock',
+            img: 'assests/guitar.png',
+            weakness: 'paper'
+        },
+        {
+            name: 'paper',
+            img: 'assests/toilet-paper.png',
+            weakness: 'scissors'
+        },
+        {
+            name: 'scissors',
+            img: 'assests/swords.png',
+            weakness: 'rock'
+        }
+    ],
+    challengeOptions: [
+        {
+            name: 'rock',
+            img: 'https://bit.ly/2wQwmYG',
+            weakness: 'paper'
+        },
+        {
+            name: 'paper',
+            img: 'https://bit.ly/2wQwmYG',
+            weakness: 'scissors'
+        },
+        {
+            name: 'scissors',
+            img: 'https://bit.ly/2wQwmYG',
+            weakness: 'rock'
+        },
+        {
+            name: 'stapler',
+            img: 'https://bit.ly/2wQwmYG',
+            weakness: 'scissors'
+        },
+        {
+            name: 'trash',
+            img: 'https://bit.ly/2wQwmYG',
+            weakness: 'rock'
+        }
+    ]
+};
+
+
 // Event Listeners
 // Functions
